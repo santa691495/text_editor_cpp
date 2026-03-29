8374dd2db85d57b7f5b8528d70a2219d8c4ff180
Christian Dimalanta <christiandimalanta20082@gmail.com>
2026-03-29 12:40:10 +0800

    Wrote tests for status text displaying. PASSED

--- tests/UI/display/unittest.cpp
+++ tests/UI/display/unittest.cpp
@@ -30,7 +30,7 @@ TEST(Display, RenderBuffer){
 TEST(Display, RenderCommandMode){
 	
 	bool cmd_mode = true;	
-
+	
 	std::string buffer_str = "TEST COMMAND MODE\npress any key to stop command"
 		" mode in this test.";
 
@@ -48,7 +48,55 @@ TEST(Display, RenderCommandMode){
 
 	char test_status = getch();
 	
+	clear();
 	endwin();
 
 	ASSERT_EQ(test_status, 'y');	
 }
+
+TEST(Display, RenderCommandStatus){
+		
+	std::string buffer_str = "TEST COMMAND STATUS\npress any key to stop"
+		" this test."
+		"\n\ntest must show the status"
+		" string provided in the test.";
+
+	std::string status_str = "success : write file";
+	bool cmd_mode = false;
+
+	//setup environment
+	Display display_obj;
+	display_obj.render_buffer(buffer_str);
+	
+	display_obj.render_cmd_status(status_str, cmd_mode);
+
+	getch();
+	cmd_mode = true;
+	
+	clear();
+	printw("%s", "test passed? y/n : ");
+	refresh();
+
+	char test_status = getch();
+	
+	clear();
+	endwin();
+
+	ASSERT_EQ(test_status, 'y');	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
