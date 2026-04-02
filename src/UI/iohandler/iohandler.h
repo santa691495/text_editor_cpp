2d0ae4df7002c369f513f33f3877f39b85d580d0
Christian Dimalanta <christiandimalanta20082@gmail.com>
2026-04-02 20:02:40 +0800

    fixed file doc

--- src/UI/iohandler/iohandler.h
+++ src/UI/iohandler/iohandler.h
@@ -8,21 +8,13 @@
 IOHandler
 
 	Responsible for resolving user input using external library functions
-	( getch() from the ncurses library) into either a command or buffer input,
-	and responsible for converting diagnostic data from CommandRunner into a
-	format (primarily std::string) digestible by the Display object.
-
+	( getch() from the ncurses library), returning an InputEvent struct 
+	containing the input and its type represented by an InputType enum.
 
 */
 
 #ifndef IO_HANDLER
 #define IO_HANDLER
-//TODO:
-// IOHandler must be stateless and only handle input.
-//it must not store state.
-// editor state could be stored elsewhere
-// parsing must be handled by display
-//needs to return what type of input (make input event struct with enum class Type)
 
 class IOHandler {
 	public:	
