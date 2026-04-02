2b0c1ea02581ef08cf36cd370d9dfb9dd924c564
Christian Dimalanta <christiandimalanta20082@gmail.com>
2026-04-02 13:09:05 +0800

    Wrote test for modify_buffer and fixed deps again

--- integration_tests/modify_buffer/integration.cpp
+++ integration_tests/modify_buffer/integration.cpp
@@ -4,6 +4,43 @@
 #include "gapbuffer.h"
 #include <ncurses.h>
 
-TEST(ModifyBuffer, InsertToBuffer){
+TEST(ModifyBuffer, InsertOnly){
+	//setup ======================
+	initscr();
+	keypad(stdscr, TRUE);
+	raw();	
 	
+	std::string buffer_text = "qwerty";
+	std::string expected_text = "qwertya";
+	
+	Display display_handler;
+	IOHandler io_handler;
+	GapBuffer gap_buffer;		
+
+	for(auto& ch : buffer_text){
+		gap_buffer.insert(ch);
+	}
+	
+	//======================	
+			
+	display_handler.render_buffer(buffer_text);
+	io_handler.handle_input(); //input 'a' in the test		
+	
+	for(auto& ch : io_handler.get_buffer_str()){
+		gap_buffer.insert(ch);
+	}
+
+	endwin();
+
+	ASSERT_EQ(gap_buffer.get_text(), expected_text);
+
 }
+/*
+TEST(ModifyBuffer, MoveHorizontalAndInsert){
+
+}
+
+TEST(ModifyBuffer, MoveVerticalAndInsert){
+
+}	
+*/
