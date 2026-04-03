2b6d245eb29a23a6bd2a2bede0089920dd9f39ae
Christian Dimalanta <christiandimalanta20082@gmail.com>
2026-04-03 20:23:49 +0800

    Fixed deps for open_file integration test

---
+++ integration_tests/open_file/integration.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include <ncurses.h>
+#include "display.h"
+#include "iohandler.h"
+#include "cmdrunner.h"
+#include "cmdparser.h"
+#include "gapbuffer.h"
+#include "filemanager.h"
+#include "editorstate.h"
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+TEST(OpenFile, OpenFile){
+
+
+}
