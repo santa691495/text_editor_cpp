4351b0de2bb01b2c25006b7d81989feb8555b851
Christian Dimalanta <christiandimalanta20082@gmail.com>
2026-04-03 12:19:35 +0800

    Created class EditorState and fixed deps for iohandler and display

---
+++ src/editorstate/editorstate.cpp
@@ -0,0 +1,1 @@
+#include "editorstate.h"
---
+++ src/editorstate/editorstate.h
@@ -0,0 +1,12 @@
+#ifndef EDITOR_STATE
+#define EDITOR_STATE
+
+class EditorState {
+    public:
+        //normal
+        bool cmd_mode = false;
+        bool running = true;
+
+};
+
+#endif 
