748a5b629eff388eae5b003ef76afa3e5d88683e
Christian Dimalanta <christiandimalanta20082@gmail.com>
2026-04-03 15:46:18 +0800

    Fixed tests for modify_buffer

--- integration_tests/modify_buffer/integration.cpp
+++ integration_tests/modify_buffer/integration.cpp
@@ -3,14 +3,13 @@
 #include "iohandler.h"
 #include "gapbuffer.h"
 #include <ncurses.h>
+#include <vector>
 
 TEST(ModifyBuffer, InsertOnly){
 	// === setup ======================
 	initscr();
 	keypad(stdscr, TRUE);
 	raw();	
-
-	// ================================
 
 	IOHandler io_handler;
 	GapBuffer gap_buffer;
@@ -23,36 +22,53 @@ TEST(ModifyBuffer, InsertOnly){
 		gap_buffer.insert(ch);
 	}
 
-	std::string buffer_str = gap_buffer.get_text();
 
-	display_handler.render_buffer(buffer_str);
 
-	size_t input_loops = 9;
-	
-	for(size_t i = 0; i < input_loops; ++i){
+	display_handler.render_buffer(init_buffer_str);
+	std::string insert_str = "Copy This";
+
+	for(char& ch : insert_str){
+		ungetch(ch);
 		InputEvent input = io_handler.get_input();
+
+		EXPECT_EQ(input.input_ch, ch);
+		EXPECT_EQ(input.type, InputType::character);
 
 		if(input.type == InputType::character){
 			gap_buffer.insert(input.input_ch);
 		}
-	}
+		
+		std::string buffer_str = gap_buffer.get_text();
+		display_handler.render_buffer(buffer_str);
+	} 
+
+
+
+	// === get internal buffer output ====
+	move(0,0);
+
+	char buffer_internal[50];
+	innstr(buffer_internal, expected_str.size());
+
+	std::string buffer_internal_stdstr(buffer_internal);
+
+
 
 	// === teardown ======================
 
 	endwin();
 
-	// ===================================
 
-	ASSERT_EQ(gap_buffer.get_text(), expected_str);
+
+	ASSERT_EQ(buffer_internal_stdstr, expected_str);
 }
-
+//FIXME
 TEST(ModifyBuffer, MoveLeftAndInsert){
 	// === setup ======================
 	initscr();
 	keypad(stdscr, TRUE);
 	raw();	
 
-	// ================================
 	IOHandler io_handler;
 	GapBuffer gap_buffer;
 	Display display_handler;
@@ -64,32 +80,38 @@ TEST(ModifyBuffer, MoveLeftAndInsert){
 		gap_buffer.insert(ch);
 	}
 
-	std::string buffer_str = gap_buffer.get_text();
+	std::vector<int> input_chars = {KEY_LEFT, 'A'};
 
-	display_handler.render_buffer(buffer_str);
+	display_handler.render_buffer(init_buffer_str);
 
-	size_t input_loops = 2;
-	
-	for(size_t i = 0; i < input_loops; ++i){
+	for(int& ch : input_chars){
+		ungetch(ch);
 		InputEvent input = io_handler.get_input();
 
 		if(input.type == InputType::character){
 			gap_buffer.insert(input.input_ch);
-		} else if(input.type == InputType::arrow_left){
+		} else if (input.type == InputType::arrow_left){
 			display_handler.move_cursor_left();
 			gap_buffer.move_left();
 		}
+
+		std::string buffer_str = gap_buffer.get_text();
+		display_handler.render_buffer(buffer_str);
 	}
 
+	// === get internal buffer output ====
+	move(0,0);
+
+	char buffer_internal[50];
+	innstr(buffer_internal, expected_str.size());
+
+	std::string buffer_internal_stdstr(buffer_internal);
 
 	// === teardown ======================
 
 	endwin();
 
-	// ===================================
-
-	ASSERT_EQ(gap_buffer.get_text(), expected_str);
-
+	ASSERT_EQ(buffer_internal_stdstr, expected_str);
 }
 
 TEST(ModifyBuffer, MoveRightAndInsert){
@@ -97,8 +119,6 @@ TEST(ModifyBuffer, MoveRightAndInsert){
 	initscr();
 	keypad(stdscr, TRUE);
 	raw();	
-
-	// ================================
 
 	IOHandler io_handler;
 	GapBuffer gap_buffer;
@@ -111,33 +131,38 @@ TEST(ModifyBuffer, MoveRightAndInsert){
 		gap_buffer.insert(ch);
 	}
 
-	std::string buffer_str = gap_buffer.get_text();
 
-	display_handler.render_buffer(buffer_str);
+	std::vector<int> input_chars = {KEY_RIGHT, 'A'};
 
-	size_t input_loops = 2;
+	display_handler.render_buffer(init_buffer_str);
 
-	//to make cursor start at last char in buffer_str
-	display_handler.move_cursor_left();
-	gap_buffer.move_left(); 
-	
-	for(size_t i = 0; i < input_loops; ++i){
+	for(int& ch : input_chars){
+		ungetch(ch);
 		InputEvent input = io_handler.get_input();
 
 		if(input.type == InputType::character){
 			gap_buffer.insert(input.input_ch);
-		} else if(input.type == InputType::arrow_right){
+		} else if (input.type == InputType::arrow_right){
 			display_handler.move_cursor_right();
 			gap_buffer.move_right();
 		}
+
+		std::string buffer_str = gap_buffer.get_text();
+		display_handler.render_buffer(buffer_str);
 	}
+
+	// === get internal buffer output ====
+	move(0,0);
+
+	char buffer_internal[50];
+	innstr(buffer_internal, expected_str.size());
+
+	std::string buffer_internal_stdstr(buffer_internal);
+
 	// === teardown ======================
 	endwin();
 
-	// ===================================
-
-	ASSERT_EQ(gap_buffer.get_text(), expected_str);
-	
+	ASSERT_EQ(buffer_internal_stdstr, expected_str);
 }
 
 TEST(ModifyBuffer, Backspace){
@@ -146,42 +171,43 @@ TEST(ModifyBuffer, Backspace){
 	keypad(stdscr, TRUE);
 	raw();	
 
-	// ================================
-
 	IOHandler io_handler;
 	GapBuffer gap_buffer;
 	Display display_handler;
 
-	std::string init_buffer_str = "Delete This Entire Line";
+	std::string init_buffer_str = "DeleteAllOfMe";
 	std::string expected_str = "";
 
 	for(char& ch : init_buffer_str){
 		gap_buffer.insert(ch);
 	}
-	
-	while(gap_buffer.get_text() != ""){
-		std::string buffer_str = gap_buffer.get_text();
-		display_handler.render_buffer(buffer_str);
 
+	display_handler.render_buffer(init_buffer_str);
+
+	for(char& ch : init_buffer_str){
+		ungetch(KEY_BACKSPACE);
 		InputEvent input = io_handler.get_input();
 
-		if(input.type == InputType::character){
-			gap_buffer.insert(input.input_ch);
-		} else if(input.type == InputType::arrow_right){
-			display_handler.move_cursor_right();
-			gap_buffer.move_right();
-		} else if(input.type == InputType::arrow_left){
-			display_handler.move_cursor_left();
-			gap_buffer.move_left();
-		} else if(input.type == InputType::backspace){
+		if (input.type == InputType::backspace){
 			gap_buffer.backspace();
 		}
+
+		std::string buffer_str = gap_buffer.get_text();
+		display_handler.render_buffer(buffer_str);
 	}
+
+	// === get internal buffer output ====
+	move(0,0);
+
+	char buffer_internal[50];
+	innstr(buffer_internal, expected_str.size());
+
+	std::string buffer_internal_stdstr(buffer_internal);
 
 	// === teardown ======================
 	endwin();
 
 	// ===================================
 
-	ASSERT_EQ(gap_buffer.get_text(), expected_str);
+	ASSERT_EQ(buffer_internal_stdstr, expected_str);
 }
