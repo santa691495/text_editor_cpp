50553174334277fc709361226b6fea9e2c8ceb0c
Christian Dimalanta <christiandimalanta20082@gmail.com>
2026-04-11 11:40:51 +0800

    Tested class Display methods manually

--- tests/UI/display/manual.cpp
+++ tests/UI/display/manual.cpp
@@ -1,27 +1,26 @@
 #include "gtest/gtest.h"
 #include <ncurses.h>
 #include "display.h"
-#include <string>
 
 //Manual testing for Display
 
 TEST(Display, ManualCommandMode){
     initscr();
-    
+
     Display display_handler;
     display_handler.render_cmd_mode();
 
-    //print and check manually if it prints on desired area
-    
     printw("%s", "This Must Be somewhere in the middle !");
     refresh();
 
     getch();
+    getch();
 
     clear();
     mvprintw(0,0, "Test passed? y/n:");
+    refresh();
     char input = getch();
-    
+
     clear();
     refresh();
     endwin();
@@ -43,6 +42,50 @@ TEST(Display, ManualCommandStatus){
     mvprintw(0,0, "Test passed? y/n:");
     refresh();
     char input = getch();
+
+    clear();
+    endwin();
+
+    ASSERT_EQ(input, 'y');
+}
+
+TEST(Display, MoveCursor){
+    initscr();
+    cbreak();
+    keypad(stdscr, TRUE);
+    Display display_handler;
+
+    clear();
+    printw("%s", "This is sample text for Cursor movement test");
+    refresh();
+
+    for(int i = 0; i < 10; ++i){
+        int in = getch();
+
+        if(in == KEY_LEFT){
+            int curs_y, curs_x;
+
+            getyx(stdscr, curs_y, curs_x);
+
+            move(curs_y, curs_x-1);
+            refresh();
+
+        } else if (in == KEY_RIGHT){
+            int curs_y, curs_x;
+
+            getyx(stdscr, curs_y, curs_x);
+
+            move(curs_y, curs_x+1);
+            refresh();
+        } else {
+            break;
+        }
+    }
+
+    clear();
+    mvprintw(0,0, "Test passed? y/n:");
+    refresh();
+    int input = getch();
 
     endwin();
 
