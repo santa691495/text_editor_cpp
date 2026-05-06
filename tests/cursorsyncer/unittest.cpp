78cdd804dbd11cea3f450cfb6e9e5361e8c799c5
santa691495 <christiandimalanta20082@gmail.com>
2026-05-06 20:12:40 +0800

    Created Tests for class CursorSyncer for edge cases

--- tests/cursorsyncer/unittest.cpp
+++ tests/cursorsyncer/unittest.cpp
@@ -172,3 +172,145 @@ TEST(CursorSync, MoveRight){
     ASSERT_EQ(new_posx, expected_new_posx);
     ASSERT_EQ(new_posy, expected_new_posy);
 }   
+
+TEST(CursorSync, MoveRightOnEdge){
+    initscr();      
+
+    GapBuffer test_gbuffer;
+    Display test_display;
+    CursorSyncer test_cursor_syncer(test_gbuffer, test_display);
+
+    std::string init_buffer_str = "QWE\nRTY";
+    std::string expected_buffer_str = "QWE\nRTY!";
+    int expected_new_posx = 3;
+    int expected_new_posy = 1;
+
+    for(char& ch : init_buffer_str){
+        test_gbuffer.insert(ch);
+    }   
+
+    move(1,3);
+
+    test_cursor_syncer.move_right();
+
+    int new_posx, new_posy;
+    getyx(stdscr, new_posy, new_posx);
+
+    test_gbuffer.insert('!');
+
+    endwin();   
+
+    ASSERT_EQ(test_gbuffer.get_text(), expected_buffer_str);
+    ASSERT_EQ(new_posx, expected_new_posx);
+    ASSERT_EQ(new_posy, expected_new_posy);
+}
+
+TEST(CursorSync, MoveLeftOnStart){
+    initscr();      
+
+    GapBuffer test_gbuffer;
+    Display test_display;
+    CursorSyncer test_cursor_syncer(test_gbuffer, test_display);
+
+    std::string init_buffer_str = "QWE\nRTY";
+    std::string expected_buffer_str = "!QWE\nRTY";
+    int expected_new_posx = 0;
+    int expected_new_posy = 0;
+
+    for(char& ch : init_buffer_str){
+        test_gbuffer.insert(ch);
+    }       
+
+    for(char ch : init_buffer_str){
+        test_gbuffer.move_left();
+    }
+
+    move(0,0);
+
+    test_cursor_syncer.move_left();
+
+    int new_posx, new_posy;
+    getyx(stdscr, new_posy, new_posx);
+
+    test_gbuffer.insert('!');
+
+    endwin();   
+
+    EXPECT_EQ(new_posy, expected_new_posy);
+    EXPECT_EQ(new_posx, expected_new_posx);
+    ASSERT_EQ(test_gbuffer.get_text(), expected_buffer_str);
+}   
+
+TEST(CursorSync, MoveStartlnUpOnTop){
+    initscr();  
+
+    GapBuffer test_gbuffer;
+    Display test_display;
+    CursorSyncer test_cursor_syncer(test_gbuffer, test_display);
+
+    std::string init_buffer_str = "QWE\nRTY";
+    std::string expected_buffer_str = "Q!WE\nRTY";
+    int expected_new_posx = 1;
+    int expected_new_posy = 0;
+
+    for(char& ch : init_buffer_str){
+        test_gbuffer.insert(ch);
+    }   
+
+    for(char& ch: init_buffer_str){
+        test_gbuffer.move_left();
+    }   
+
+    move(0,1);
+    test_gbuffer.move_right();
+
+    test_cursor_syncer.move_startln_up();
+
+    int new_posx, new_posy;
+    getyx(stdscr, new_posy, new_posx);
+
+    test_gbuffer.insert('!');
+
+    endwin();   
+
+    ASSERT_EQ(test_gbuffer.get_text(), expected_buffer_str);
+    ASSERT_EQ(new_posx, expected_new_posx);
+    ASSERT_EQ(new_posy, expected_new_posy);
+}
+
+TEST(CursorSync, MoveStartlnDownOnBottom){
+    initscr();      
+
+    GapBuffer test_gbuffer;
+    Display test_display;
+    CursorSyncer test_cursor_syncer(test_gbuffer, test_display);
+
+    std::string init_buffer_str = "QWE\nRTY";
+    std::string expected_buffer_str = "QWE\nRT!Y";
+    int expected_new_posx = 2;
+    int expected_new_posy = 1;
+
+    for(char& ch : init_buffer_str){
+        test_gbuffer.insert(ch);
+    }   
+
+    test_gbuffer.move_left();
+
+    move(1,2);
+
+    test_cursor_syncer.move_startln_down();
+
+    int new_posx, new_posy;
+    getyx(stdscr, new_posy, new_posx);
+
+    test_gbuffer.insert('!');
+
+    endwin();   
+
+
+    ASSERT_EQ(test_gbuffer.get_text(), expected_buffer_str);
+    ASSERT_EQ(new_posx, expected_new_posx);
+    ASSERT_EQ(new_posy, expected_new_posy);
+
+}
+
