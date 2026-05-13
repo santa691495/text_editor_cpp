95d464d3f7d91a420fc540fbf55a65b81c5be965
santa691495 <christiandimalanta20082@gmail.com>
2026-05-13 11:54:51 +0800

    QUICK REFACTOR on GapBuffer::move_right_line()

--- src/buffer/gapbuffer.cpp
+++ src/buffer/gapbuffer.cpp
@@ -11,9 +11,9 @@ void GapBuffer::move_left(){
 	if(gap_start == &buffer[0]){
 		return;
 	}
-	
+
 	char* target {gap_start-1};
-	
+
 	char* rightmost {gap_end-1};
 	*rightmost = *target;
 
@@ -27,9 +27,9 @@ void GapBuffer::move_right(){
 	if(gap_end == buffer_end){
 		return;
 	}
-	
+
 	char* target {gap_end};
-	
+
 	char* leftmost {gap_start};
 	*leftmost = *target;
 
@@ -39,19 +39,19 @@ void GapBuffer::move_right(){
 
 void GapBuffer::grow(){
 	size_t new_gap_size {buffer.size()};
-	
+
 	char* old_base = buffer.data();
 	char* old_end = old_base + buffer.size();
 
-	std::vector<char> new_buffer(buffer.size()*2); 
+	std::vector<char> new_buffer(buffer.size()*2);
 	char* new_base = new_buffer.data();
 
 	size_t left_size = gap_start - old_base;
 	size_t right_size = old_end - gap_end;
-	
+
 	char* new_gap_start = new_base + left_size;
 	char* new_gap_end  = new_base + new_buffer.size() - right_size;
-	 
+
 	for(char* src = old_base, *dst = new_base; src < gap_start; ++src, ++dst){
 		*dst = *src;
 	}
@@ -68,32 +68,32 @@ void GapBuffer::grow(){
 
 bool GapBuffer::is_growable(){
 	std::string current_text { get_text() };
-	
+
 	if(current_text.size() < get_current_size()){
 		return false;
-	} 
+	}
 
 	return true;
 }
 
 std::string GapBuffer::get_text(){
 	std::string text;
-	
+
 	size_t current_gap_size = gap_end - gap_start;
 	size_t reserve_amnt = buffer.size() - current_gap_size;
 	text.reserve(reserve_amnt);
-	
+
 	char* buffer_start = buffer.data();
 	char* buffer_end = buffer.data() + buffer.size();
-	
+
 	size_t left_size = gap_start - buffer_start;
 	size_t right_size = buffer_end - gap_end;
-	
+
 	text.append(buffer_start, left_size);
 	text.append(gap_end, right_size);
-	
+
 	return text;
-}	
+}
 
 size_t GapBuffer::get_current_size(){
 	size_t current_size {buffer.size()};
@@ -104,7 +104,7 @@ void GapBuffer::backspace(){
 	if(gap_start == &buffer[0]){
 		return;
 	}
-	
+
 	gap_start -= 1;
 }
 
@@ -119,13 +119,14 @@ bool GapBuffer::move_right_line(){
 		return false;
 	}
 
-	//finder can only go up to \n,
 	char* finder = gap_end;
-	for(; *finder != '\n'; ++finder){
-		if(finder == buffer.data() + buffer.size()){
-			return false;
-		}
-	} 
+	while(*finder != '\n' && finder != buffer.data() + buffer.size()){
+	    ++finder;
+	}
+
+	if(finder == buffer.data() + buffer.size()){
+	    return false;
+	}
 
 	while(gap_end != finder+1){
 		move_right();
@@ -148,7 +149,7 @@ bool GapBuffer::move_left_line() {
         return false;
     }
 
-    char* line_end = finder; 
+    char* line_end = finder;
     --finder;
 
     while (finder >= buffer.data() && *finder != '\n') {
@@ -180,22 +181,22 @@ bool GapBuffer::move_startln_down(){
 bool GapBuffer::is_at_line_start() {
 	if(gap_start == buffer.data()){
 		return true;
-	} 
+	}
 	if(*(gap_start-1) == '\n'){
 		return true;
-	}	
+	}
 	return false;
-}	
+}
 
 bool GapBuffer::is_at_line_edge() {
 	if(gap_end == buffer.data() + buffer.size()){
 		return true;
-	} 
+	}
 	if(*gap_end == '\n'){
 		return true;
-	}	
+	}
 	return false;
-}	
+}
 
 void GapBuffer::move_to_start(){
 	while(gap_start != buffer.data()){
